d30954dd71a4c715ee9b76b5753d33a2f9d6d2f8
Deb Mukherjee <deblina@cs.uchicago.edu>
2019-10-11 22:05:07 -0500

    All but task 7

--- pa1/sir.py
+++ pa1/sir.py
@@ -1,7 +1,7 @@
 '''
 Epidemic modelling
 
-YOUR NAME
+Deblina Mukherjee
 
 Functions for running a simple epidemiological simulation
 '''
@@ -26,156 +26,227 @@ def count_ever_infected(city):
     '''
 
     # YOUR CODE HERE
-
+    count = 0 
+    for y in city:
+        if y[0] == 'I':
+            count += 1
+        elif y == 'R':
+            count += 1
     # REPLACE -1 WITH THE APPROPRIATE INTEGER
-    return -1
+    return count
 
 
 def has_an_infected_neighbor(city, position):
-    '''
-    Determine whether a person has an infected neighbor
+#     '''
+#     Determine whether a person has an infected neighbor
 
-    Inputs:
-      city (list): the state of all people in the simulation at the
-        start of the day
-      position (int): the position of the person to check
+#     Inputs:
+#       city (list): the state of all people in the simulation at the
+#         start of the day
+#       position (int): the position of the person to check
 
-    Returns:
-      True, if the person has an infected neighbor, False otherwise.
-    '''
-
+#     Returns:
+#       True, if the person has an infected neighbor, False otherwise.
+#     '''
+    
     assert city[position] == "S"
 
-    # YOUR CODE HERE
 
-    # REPLACE None WITH THE APPROPRIATE BOOLEAN VALUE
-
-    return None
-
+# YOUR CODE HERE
+    if len(city) == 1: #list of one 
+        return False 
+    elif position == 0: #first element -- not sure about this 
+        if city[1][0] == "I": 
+          return True
+        else: 
+          return False 
+    elif position == len(city) - 1: #last element -- not sure about this 
+        if city[-2][0] == "I":
+          return True 
+        else: 
+            return False 
+    else: 
+        if city[position + 1][0] == "I": 
+          return True
+        elif city[position - 1][0] == "I":
+          return True 
+        else: 
+            return False 
 
 def gets_infected_at_position(city, position, infection_rate):
-    '''
-    Determine whether the person at the specified position gets
-    infected.
+#     '''
+#     Determine whether the person at the specified position gets
+#     infected.
 
-    Inputs:
-      city (list): the state of all people in the simulation at the
-        start of the day
-      position (int): the position of the person to check
-      infection_rate (float): the chance of getting infected given an
-        infected neighbor
+#     Inputs:
+#       city (list): the state of all people in the simulation at the
+#         start of the day
+#       position (int): the position of the person to check
+#       infection_rate (float): the chance of getting infected given an
+#         infected neighbor
 
 
-    Returns (boolean):
-      True, if the person would become infected, False otherwise.
-    '''
-    # This function should only be called when the person at position
-    # is susceptible to infection.
-    assert city[position] == "S"
+#     Returns (boolean):
+#       True, if the person would become infected, False otherwise.
+#     '''
+#     # This function should only be called when the person at position
+#     # is susceptible to infection.
+#     assert city[position] == "S"
 
-    # YOUR CODE HERE
-
-    # REPLACE None WITH THE APPROPRIATE BOOLEAN VALUE
-    return None
+#     # YOUR CODE HERE
+    if has_an_infected_neighbor(city, position) == True: 
+        if random.random() < infection_rate: 
+            return True
+        else: 
+            return False 
+    else: 
+        # REPLACE None WITH THE APPROPRIATE BOOLEAN VALUE
+        return False     
 
 
 def advance_person_at_position(city, position,
-                               infection_rate, days_contagious):
-    '''
-    Compute the next state for the person at the specified position.
+                                infection_rate, days_contagious):
+#     '''
+#     Compute the next state for the person at the specified position.
 
-    Inputs:
-      city (list): the state of all people in the simulation at the
-        start of the day
-      position (int): the position of the person to check
-      infection_rate (float): the chance of getting infected given an
-        infected neighbor
-      days_contagious (int): the number of a days a person is infected
+#     Inputs:
+#       city (list): the state of all people in the simulation at the
+#         start of the day
+#       position (int): the position of the person to check
+#       infection_rate (float): the chance of getting infected given an
+#         infected neighbor
+#       days_contagious (int): the number of a days a person is infected
 
-    Returns: (string) disease state of the person after one day
-    '''
+#     Returns: (string) disease state of the person after one day
+#     '''
 
-    # YOUR CODE HERE
-
-    # REPLACE None WITH THE APPROPRIATE STRING
-    return None
-
+#     # YOUR CODE HERE
+    if city[position] == "S": 
+        if gets_infected_at_position(city, position, infection_rate): 
+            return "I0"
+        else: 
+            return 'S'
+    elif city[position][0] == "I":
+        days = int(city[position][1:])
+        if days + 1 < days_contagious: 
+            return "I" + str(days + 1)
+        else: 
+            return 'R'
+    elif city[position] == "R": 
+   # REPLACE None WITH THE APPROPRIATE STRING
+        return 'R'
 
 def simulate_one_day(starting_city, infection_rate, days_contagious):
-    '''
-    Move the simulation forward a single day.
+#     '''
+#     Move the simulation forward a single day.
 
-    Inputs:
-      starting_city (list): the state of all people in the simulation at the
-        start of the day
-      infection_rate (float): the chance of getting infected given an
-        infected neighbor
-      days_contagious (int): the number of a days a person is infected
-    Returns:
-      new_city (list): disease state of the city after one day
-    '''
+#     Inputs:
+#       starting_city (list): the state of all people in the simulation at the
+#         start of the day
+#       infection_rate (float): the chance of getting infected given an
+#         infected neighbor
+#       days_contagious (int): the number of a days a person is infected
+#     Returns:
+#       new_city (list): disease state of the city after one day
+#     '''
 
-    # YOUR CODE HERE
+#     # YOUR CODE HERE
+    end_city = []
+    for i in range(len(starting_city)): #could also use ennumerate 
+        end_city.append(advance_person_at_position(starting_city,i,
+            infection_rate, days_contagious))
+    return end_city 
 
-    # REPLACE None WITH THE APPROPRIATE LIST OF STRINGS
-    return None
-
+#     # REPLACE None WITH THE APPROPRIATE LIST OF STRINGS
 
 def run_simulation(starting_city, random_seed, max_num_days,
                    infection_rate, days_contagious):
-    '''
-    Run the entire simulation for up to the specified maximum number
-    of days.
+#     '''
+#     Run the entire simulation for up to the specified maximum number
+#     of days.
 
-    Inputs:
-      starting_city (list): the state of all people in the city at the
-        start of the simulation
-      random_seed (int): the random seed to use for the simulation
-      max_num_days (int): the maximum days of the simulation
-      infection_rate (float): the chance of getting infected given an
-        infected neighbor
-      days_contagious (int): the number of a days a person is infected
+#     Inputs:
+#       starting_city (list): the state of all people in the city at the
+#         start of the simulation
+#       random_seed (int): the random seed to use for the simulation
+#       max_num_days (int): the maximum days of the simulation
+#       infection_rate (float): the chance of getting infected given an
+#         infected neighbor
+#       days_contagious (int): the number of a days a person is infected
 
-    Returns tuple (list of strings, int): the final state of the city
-      and the number of days actually simulated.
-    '''
+#     Returns tuple (list of strings, int): the final state of the city
+#       and the number of days actually simulated.
+#     '''
     assert max_num_days >= 0
 
-    # YOUR CODE HERE
+#     # YOUR CODE HERE
+    random.seed(random_seed)
+    post_sim_1day = [] #list of all cities
+    post_sim_1day.append(simulate_one_day(starting_city, 
+        infection_rate, days_contagious))
+    num_days = 1
+    if post_sim_1day[0] == starting_city: 
+        return(post_sim_1day[0], num_days)
+    else: 
+        for i in range(max_num_days-1): 
+            num_days += 1
+            post_sim_1day.append(simulate_one_day(post_sim_1day[-1], 
+                infection_rate, days_contagious))
+            if post_sim_1day[-1] == post_sim_1day[-2]: 
+                return (post_sim_1day[-1], num_days)
+        return (post_sim_1day[-1], num_days)
 
-    # REPLACE (None, None) WITH THE APPROPRIATE TUPLE
-    #  (city, number of days simulated)
-    return (None, None)
+#     # REPLACE (None, None) WITH THE APPROPRIATE TUPLE
+#     #  (city, number of days simulated)
 
 
 def calc_avg_num_newly_infected(
-        starting_city, random_seed, max_num_days,
-        infection_rate, days_contagious, num_trials):
-    '''
-    Conduct N trials with the specified infection probability and
-    calculate the number of people on average get infected over time.
+         starting_city, random_seed, max_num_days,
+         infection_rate, days_contagious, num_trials):
+#     '''
+#     Conduct N trials with the specified infection probability and
+#     calculate the number of people on average get infected over time.
 
-    Inputs:
-      starting_city (list): the state of all people in the city at the
-        start of the simulation
-      random_seed (int): the starting random seed. Use this value for
-        the FIRST simulation, and then increment it once for each
-        subsequent run.
-      max_num_days (int): the maximum days of the simulation
-      infection_rate (float): the chance of getting infected given an
-        infected neighbor
-      days_contagious (int): the number of a days a person is infected
-      num_trials (int): the number of trials to run
+#     Inputs:
+#       starting_city (list): the state of all people in the city at the
+#         start of the simulation
+#       random_seed (int): the starting random seed. Use this value for
+#         the FIRST simulation, and then increment it once for each
+#         subsequent run.
+#       max_num_days (int): the maximum days of the simulation
+#       infection_rate (float): the chance of getting infected given an
+#         infected neighbor
+#       days_contagious (int): the number of a days a person is infected
+#       num_trials (int): the number of trials to run
 
-    Returns (float): the average number of people infected over time
-    '''
+#     Returns (float): the average number of people infected over time
+#     '''
     assert max_num_days >= 0
     assert num_trials > 0
 
-    # YOUR CODE HERE
-
-    # REPLACE -1.0 WITH THE APPROPRIATE FLOATING POINT VALUE
-    return -1.0
+#     # YOUR CODE HERE
+def calc_avg_num_newly_infected(
+         starting_city, random_seed, max_num_days,
+         infection_rate, days_contagious, num_trials):
+    start_inf = count_ever_infected(starting_city)
+    all_sim = []
+    diff = []
+    for i in range(num_trials): 
+        print(start_inf, "starting infected")
+        print(random_seed, "random")
+        all_sim.append(run_simulation(starting_city, random_seed, max_num_days, 
+                                      infection_rate, days_contagious)[0])
+        print(all_sim[i])
+        end_inf = int(count_ever_infected(all_sim[-1]))
+        print(end_inf, "ever infected")
+        print(end_inf - start_inf, "new infected")
+        if (end_inf > start_inf) :
+            diff.append(end_inf - start_inf)
+        start_inf = end_inf 
+        random_seed = random_seed + 1
+        diffsum = sum(diff)
+    return diffsum/num_trials
+#     # REPLACE -1.0 WITH THE APPROPRIATE FLOATING POINT VALUE
 
 
 ################ Do not change the code below this line #######################
